1445a9403627b527d562b2f5ff1f78005e9a39db
Nitesh Tambore <v-nitesh.tambore@choreograph.com>
2025-11-26 16:37:07 +0530

    commit 1 - before dvc

---
+++ csv_generator.py
@@ -0,0 +1,26 @@
+import os
+import csv
+
+# Folder and file path
+data_folder = "data"
+file_path = os.path.join(data_folder, "sample_data.csv")
+
+# Create data folder if not exists
+os.makedirs(data_folder, exist_ok=True)
+
+# Sample employee data
+employees = [
+    ["emp_id", "name", "age", "department", "salary"],
+    [1001, "Nitesh", 29, "Data Engineering", 1200000],
+    [1002, "Yogesh", 31, "Backend", 900000],
+    [1003, "Ravi", 26, "QA", 650000],
+    [1004, "Sneha", 28, "MLOps", 1100000],
+    [1005, "Priya", 24, "Analytics", 750000]
+]
+
+# Write CSV file
+with open(file_path, mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(employees)
+
+print(f"CSV file created at: {file_path}")
