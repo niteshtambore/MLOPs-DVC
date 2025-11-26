1a698930b9ad0a62647d423056a68bc8ce4ac8f4
Nitesh Tambore <v-nitesh.tambore@choreograph.com>
2025-11-26 18:10:57 +0530

    commit 5 DVC initiated with data version - 2

--- csv_generator.py
+++ csv_generator.py
@@ -1,6 +1,8 @@
 import os
 import csv
 
+import pandas as pd
+
 # Folder and file path
 data_folder = "data"
 file_path = os.path.join(data_folder, "sample_data.csv")
@@ -15,7 +17,8 @@ employees = [
     [1002, "Yogesh", 31, "Backend", 900000],
     [1003, "Ravi", 26, "QA", 650000],
     [1004, "Sneha", 28, "MLOps", 1100000],
-    [1005, "Priya", 24, "Analytics", 750000]
+    [1005, "Priya", 24, "Analytics", 750000],
+    [1006,"Nidhi",28, "Data Analysis",80000]
 ]
 
 # Write CSV file
